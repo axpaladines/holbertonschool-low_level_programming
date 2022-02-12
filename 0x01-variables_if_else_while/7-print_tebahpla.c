e87234bffb823c68b51768cce41f35898d6f0693
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-02-11 22:58:59 -0500

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - reverse alphabet
+* Return: Always 0
+*/
+
+int main(void)
+
+{
+
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
