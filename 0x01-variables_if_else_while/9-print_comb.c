174f3dce316017e35512aa1a1479224e1740d4ee
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-02-11 23:27:52 -0500

    Patience, persistence and perspiration for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
 /**
-* main - single-digital numbers
-* Return: Always 0
+ * main - single-digit numbers
+ * Return: Always 0
 */
 
 int main(void)
 
 {
-	int n = 0;
 
-	for (n = 0; n <= 10; n++)
-	{
-	putchar('0' + n);
-
-	if (n != 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-
-	putchar('\n');
-
-	return (0);
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar((n % 10) + '0');
+if (n == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
