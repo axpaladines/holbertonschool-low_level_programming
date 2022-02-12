3f15e975eb8f605ee04d5bd43c0e86357fe9e9a2
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-02-11 22:50:27 -0500

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - digit numbers
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
