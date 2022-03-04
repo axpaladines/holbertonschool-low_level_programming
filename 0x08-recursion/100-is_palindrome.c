b8cec5cf1f2173ad8c04041c13cda34e09f2edcd
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-03-03 21:24:45 -0500

    100-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,36 @@
 #include "main.h"
 
 /**
+* *to_end - function
+* @s: sting
+* Return: Always 0
+*/
+
+char *to_end(char *s)
+{
+	if (!*(s + 1))
+		return (s);
+	return (to_end(s + 1));
+}
+
+/**
+* is_palindrome_out - funtion
+* Return: Always 0
+* @s: string one
+* @s1: string two
+*/
+
+int is_palindrome_out(char *s, char *s1)
+{
+	if (*s != *s1)
+		return (0);
+	if (s >= s1)
+		return (1);
+	return (is_palindrome_out(s + 1, s1 - 1));
+}
+
+
+/**
 * is_palindrome - return string is a palindrome
 * @s: string
 * Return: Always 0
@@ -8,22 +38,13 @@
 
 int is_palindrome(char *s)
 {
-	char *s1 = s;
+	char *s1;
 
 	if (!s)
 		return (-1);
 	if (!*s)
 		return (1);
+	s1 = to_end(s);
 
-	while (*(s1 + 1))
-		s1++;
-	while (s <= s1)
-	{
-		if (*s == *s1)
-			s++, s1--;
-		else
-			return (0);
-	}
-
-	return (1);
+	return (is_palindrome_out(s, s1));
 }
