d01850734e96414d5b4c42f0aaa91835fdeb58d1
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-03-04 15:46:47 -0500

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the name
+* @argc: number of input
+* @argv: array
+* Return: Always 0
+*/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
