0cfb4c024c872cfb73f012c6c0ffa91fa84c8a01
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-03-11 20:46:58 -0500

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - check the code
+* @b: input for the function
+*
+* Return: Always 0
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+
+	return (s);
+}
