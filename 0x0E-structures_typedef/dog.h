4a8aa5b03abb0a4942b22b10f8b101e532ed4ab9
axpaladines <alexanderpaladinessivisapa@gmail.com>
2022-03-14 15:24:40 -0500

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _FILE_
+#define _FILE_
+#include <stdio.h>
+
+/**
+* struct dog - structure dog
+* @name: type char
+* @age: type float
+* @owner: type char
+*
+* Description: longer description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
